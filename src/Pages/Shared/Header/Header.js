26f393e548b09b957bd854ee1b769b01cbfbcca7
Md-Nihal <mdnihal01716@gmail.com>
2022-05-07 18:41:41 +0600

    header, footer, banner added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar bg="dark" variant="dark" sticky="top">
+                <Container className='d-flex'>
+                    <Navbar.Brand as={Link} to="/">Digital Supply Co.</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Products</Nav.Link>
+                    </Nav>
+
+                    <Nav>
+                        <Nav.Link href="#pricing">Blogs</Nav.Link>
+                        <Nav.Link href="#pricing">About us</Nav.Link>
+                        <Nav.Link href="#pricing">Login</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </>
+    );
+};
+
+export default Header;
